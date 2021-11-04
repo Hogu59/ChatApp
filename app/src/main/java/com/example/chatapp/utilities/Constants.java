5cb2080c6264deb5d15a368d1f4679b41a133d2e
Hogu59 <62333909+Hogu59@users.noreply.github.com>
2021-11-04 19:52:51 +0900

    message notification added

--- app/src/main/java/com/example/chatapp/utilities/Constants.java
+++ app/src/main/java/com/example/chatapp/utilities/Constants.java
@@ -1,5 +1,7 @@
 package com.example.chatapp.utilities;
 
+import java.util.HashMap;
+
 public class Constants {
     public static final String KEY_COLLECTION_USERS = "users";
     public static final String KEY_NAME = "name";
@@ -23,4 +25,24 @@ public class Constants {
     public static final String KEY_RECEIVER_IMAGE = "receiverImage";
     public static final String KEY_LAST_MESSAGE = "lastMessage";
     public static final String KEY_AVAILABILITY = "availability";
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+
+    public static HashMap<String, String> remoteMsgHeaders = null;
+    public static HashMap<String, String> getRemoteMsgHeaders(){
+        if(remoteMsgHeaders == null){
+            remoteMsgHeaders = new HashMap<>();
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_AUTHORIZATION,
+                    "key=AAAA-cMYNDU:APA91bGsrKdiWj2HfnaorVmclE4n6xGUSndwCyODg51Yg65HskDaQHQvES4HYPjm2rwgOxvziTotWolkT-LMSY3rG252uYWALQzTAsQkaNy-SPumZtUJVeXJJuxFlfZsC-eTmJ_TsdrB"
+            );
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_CONTENT_TYPE,
+                    "application/json"
+            );
+        }
+        return remoteMsgHeaders;
+    }
 }
